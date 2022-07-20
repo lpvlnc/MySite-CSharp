05a7bed31728bb10d60d8fdcaf6f5a3af2ad0b48
Leonardo Valença <lpvalenca@hotmail.com>
2022-07-20 00:06:59 -0300

    liberado swagger em produção

--- Portfolio/Portfolio.API/Program.cs
+++ Portfolio/Portfolio.API/Program.cs
@@ -46,15 +46,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-else
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = "swagger";
+});
 
 app.UseCors("corsapp");
 
